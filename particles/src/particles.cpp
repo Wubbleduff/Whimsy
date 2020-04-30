01278ecea92749ed011a36db14e8a88f537bc1ee
Wubbleduff <michael.s.fritz@gmail.com>
2020-04-29 22:42:12 -0700

    drawing with geometry shader

--- particles/src/particles.cpp
+++ particles/src/particles.cpp
@@ -42,20 +42,27 @@ void render();
 void end_frame();
 v2 mouse_world_position();
 
-struct Particle
+struct RenderParticle
 {
     v2 position;
-    float scale;
+    v2 scale;
+    float rotation;
     v4 color;
+};
 
+struct PhysicsParticle
+{
     v2 velocity;
+    float angular_velocity;
 };
 
 
 struct ParticleData
 {
     int num_particles;
-    Particle *particles;
+    RenderParticle *render_particles;
+    PhysicsParticle *physics_particles;
+
 
     float update_timer;
 
@@ -75,7 +82,7 @@ void reset_particle_positions()
 {
     for(int i = 0; i < particle_data->num_particles; i++)
     {
-        Particle *p = &(particle_data->particles[i]);
+        RenderParticle *p = &(particle_data->render_particles[i]);
 
         float percent = (float)i / particle_data->num_particles;
         float theta = 2.0f * PI * percent;
@@ -83,10 +90,15 @@ void reset_particle_positions()
         float random_number = (rand() % 100000) / 100000.0f;
 
         p->position = v2(cos(theta), sin(theta)) * 5.0f * random_number;
-        p->scale = 0.2f;
+        p->scale = v2(0.2f, 0.2f);
         p->color = v4(1.0f, 1.0f, 1.0f, 1.0f);
+    }
 
+    for(int i = 0; i < particle_data->num_particles; i++)
+    {
+        PhysicsParticle *p = &(particle_data->physics_particles[i]);
         p->velocity = v2();
+        p->angular_velocity = 0.0f;
     }
 }
 
@@ -94,8 +106,9 @@ void init_particles()
 {
     particle_data = (ParticleData *)calloc(1, sizeof(ParticleData));
 
-    particle_data->num_particles = 10000;
-    particle_data->particles = (Particle *)calloc(particle_data->num_particles, sizeof(Particle));
+    particle_data->num_particles = 1e5;
+    particle_data->render_particles = (RenderParticle *)calloc(particle_data->num_particles, sizeof(RenderParticle));
+    particle_data->physics_particles = (PhysicsParticle *)calloc(particle_data->num_particles, sizeof(PhysicsParticle));
 
     particle_data->tick_times_end = 0;
     particle_data->tick_times = (float *)calloc(TIME_BUFFER_COUNT, sizeof(float));
@@ -113,21 +126,22 @@ void update_particles()
 
     for(int i = 0; i < particle_data->num_particles; i++)
     {
-        Particle *p = &(particle_data->particles[i]);
+        RenderParticle *render = &(particle_data->render_particles[i]);
+        PhysicsParticle *physics = &(particle_data->physics_particles[i]);
 
         if(mouse_state(0))
         {
-            v2 to_mouse = mouse_pos - p->position;
+            v2 to_mouse = mouse_pos - render->position;
             //p->velocity += unit(to_mouse) * 0.01f;
             v2 direction = unit(to_mouse);
             float mag = 1.0f / (length(to_mouse)*length(to_mouse));
-            p->velocity += direction * mag * 0.001f;
+            physics->velocity += direction * mag * 0.001f;
         }
 
 
 
         //p->velocity -= p->velocity * 0.01f;
-        p->position += p->velocity;
+        render->position += physics->velocity;
     }
 
     ImGui::Begin("E");
@@ -163,8 +177,8 @@ void record_tick_time(float tick_time)
 void start_particles()
 {
     init_platform();
-    init_renderer();
     init_particles();
+    init_renderer();
     init_input();
     init_profiling();
 
@@ -187,9 +201,9 @@ void start_particles()
 
         int update_count = 0;
         float dt = get_dt();
-        if(dt >= 0.033f) dt = 0.033f;
+        //if(dt >= 0.033f) dt = 0.033f;
         particle_data->update_timer += dt;
-        while(particle_data->update_timer >= FRAME_TIME && update_count < MAX_UPDATES)
+        //while(particle_data->update_timer >= FRAME_TIME && update_count < MAX_UPDATES)
         {
             start_timer("tick");
 
@@ -207,7 +221,7 @@ void start_particles()
             start_timer("render");
             render();
             float render_secs = end_timer();
-            fprintf(particle_data->log_file, "render secs: %f\n", render_secs);
+            fprintf(particle_data->log_file, "render ms: %f\n", render_secs * 1e3);
 
                 
 
@@ -273,6 +287,10 @@ struct RendererData
     GLuint quad_vao;
     GLuint quad_ebo;
     GLuint quad_shader_program;
+
+    GLuint particles_vao;
+    GLuint particles_vbo;
+    GLuint particles_shader_program;
 
     GLuint default_texture;
 };
@@ -407,6 +425,73 @@ static GLuint make_texture(const char *path)
     return texture;
 }
 
+static GLuint make_shader(const char *vert_source, const char *frag_source, const char *geom_source = nullptr)
+{
+    int  success;
+    char info_log[512];
+
+    unsigned int vert_shader;
+    vert_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vert_shader, 1, &vert_source, NULL);
+    glCompileShader(vert_shader);
+    glGetShaderiv(vert_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vert_shader, 512, NULL, info_log);
+        fprintf(renderer_data->log, "SHADER ERROR: %s\n", info_log);
+    }
+
+    unsigned int frag_shader;
+    frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(frag_shader, 1, &frag_source, NULL);
+    glCompileShader(frag_shader);
+    glGetShaderiv(frag_shader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(frag_shader, 512, NULL, info_log);
+        fprintf(renderer_data->log, "SHADER ERROR: %s\n", info_log);
+    }
+
+
+    unsigned int geom_shader;
+    if(geom_source != nullptr)
+    {
+        geom_shader = glCreateShader(GL_GEOMETRY_SHADER);
+        glShaderSource(geom_shader, 1, &geom_source, NULL);
+        glCompileShader(geom_shader);
+        glGetShaderiv(geom_shader, GL_COMPILE_STATUS, &success);
+        if(!success)
+        {
+            glGetShaderInfoLog(geom_shader, 512, NULL, info_log);
+            fprintf(renderer_data->log, "SHADER ERROR: %s\n", info_log);
+        }
+    }
+    
+    check_gl_errors("compiling shaders");
+
+    GLuint program = glCreateProgram();
+    check_gl_errors("making program");
+
+    glAttachShader(program, vert_shader);
+    glAttachShader(program, frag_shader);
+    if(geom_source != nullptr) glAttachShader(program, geom_shader);
+    glLinkProgram(program);
+    check_gl_errors("linking program");
+
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if(!success)
+    {
+        glGetProgramInfoLog(program, 512, NULL, info_log);
+        fprintf(renderer_data->log, "SHADER ERROR: %s\n", info_log);
+    }
+
+    glDeleteShader(vert_shader);
+    glDeleteShader(frag_shader); 
+    check_gl_errors("deleting shaders");
+
+    return program;
+}
+
 static void init_renderer()
 {
     renderer_data = (RendererData *)calloc(1, sizeof(RendererData));
@@ -444,100 +529,194 @@ static void init_renderer()
     };
 
 
-    glGenVertexArrays(1, &renderer_data->quad_vao);
-    glBindVertexArray(renderer_data->quad_vao);
-    check_gl_errors("making vao");
-
-    GLuint vbo;
-    glGenBuffers(1, &vbo);
-    check_gl_errors("making vbo");
-
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    check_gl_errors("send vbo data");
-
-    float stride = 4 * sizeof(float);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, stride, (void *)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void *)(sizeof(v2)));
-    glEnableVertexAttribArray(1);
-    check_gl_errors("vertex attrib pointer");
-
-    glGenBuffers(1, &renderer_data->quad_ebo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, renderer_data->quad_ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-
-
-
-    const char *vert_shader_source =
-        "#version 330 core\n"
-        "layout (location = 0) in vec2 a_pos;\n"
-        "layout (location = 1) in vec2 a_uv;\n"
-        "uniform mat4 mvp;\n"
-        "out vec2 uvs;\n"
-        "void main()\n"
-        "{\n"
-        "    uvs = a_uv;\n"
-        "    gl_Position = mvp * vec4(a_pos.x, a_pos.y, 0.0f, 1.0);\n"
-        "}\0";
-
-    const char *frag_shader_source =
-        "#version 330 core\n"
-        "uniform sampler2D texture0;\n"
-        "in vec2 uvs;"
-        "out vec4 frag_color;\n"
-        "void main()\n"
-        "{\n"
-        "    frag_color = texture(texture0, uvs);\n"
-        //"    frag_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-        //"    frag_color = vec4(uvs.x, uvs.y, 0.0f, 0.0f);\n"
-        "}\0";
-
-    int  success;
-    char info_log[512];
-
-    unsigned int vert_shader;
-    vert_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vert_shader, 1, &vert_shader_source, NULL);
-    glCompileShader(vert_shader);
-    glGetShaderiv(vert_shader, GL_COMPILE_STATUS, &success);
-    if(!success)
     {
-        glGetShaderInfoLog(vert_shader, 512, NULL, info_log);
-        fprintf(renderer_data->log, "SHADER ERROR: %s\n", info_log);
+        glGenVertexArrays(1, &renderer_data->quad_vao);
+        glBindVertexArray(renderer_data->quad_vao);
+        check_gl_errors("making vao");
+
+        GLuint vbo;
+        glGenBuffers(1, &vbo);
+        check_gl_errors("making vbo");
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        check_gl_errors("send vbo data");
+
+        float stride = 4 * sizeof(float);
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, stride, (void *)0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void *)(sizeof(v2)));
+        glEnableVertexAttribArray(1);
+        check_gl_errors("vertex attrib pointer");
+
+        glGenBuffers(1, &renderer_data->quad_ebo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, renderer_data->quad_ebo);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     }
 
-    unsigned int frag_shader;
-    frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(frag_shader, 1, &frag_shader_source, NULL);
-    glCompileShader(frag_shader);
-    glGetShaderiv(frag_shader, GL_COMPILE_STATUS, &success);
-    if(!success)
+
+
+
+
     {
-        glGetShaderInfoLog(frag_shader, 512, NULL, info_log);
-        fprintf(renderer_data->log, "SHADER ERROR: %s\n", info_log);
+        glGenVertexArrays(1, &renderer_data->particles_vao);
+        glBindVertexArray(renderer_data->particles_vao);
+        check_gl_errors("making vao");
+
+        glGenBuffers(1, &renderer_data->particles_vbo);
+        check_gl_errors("making vbo");
+
+        glBindBuffer(GL_ARRAY_BUFFER, renderer_data->particles_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(RenderParticle) * particle_data->num_particles, particle_data->render_particles, GL_DYNAMIC_DRAW);
+        check_gl_errors("send vbo data");
+
+        float stride = sizeof(RenderParticle);
+        // Position
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, stride, (void *)0);
+        glEnableVertexAttribArray(0);
+        // Scale
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, stride, (void *)(sizeof(v2)));
+        glEnableVertexAttribArray(1);
+        // Rotation
+        glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, stride, (void *)(2 * sizeof(v2)));
+        glEnableVertexAttribArray(2);
+        // Color
+        glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, stride, (void *)(2 * sizeof(v2) + 1));
+        glEnableVertexAttribArray(3);
+        check_gl_errors("vertex attrib pointer");
     }
-    check_gl_errors("compiling shaders");
 
-    renderer_data->quad_shader_program = glCreateProgram();
-    check_gl_errors("making program");
 
-    glAttachShader(renderer_data->quad_shader_program, vert_shader);
-    glAttachShader(renderer_data->quad_shader_program, frag_shader);
-    glLinkProgram(renderer_data->quad_shader_program);
-    check_gl_errors("linking program");
 
-    glGetProgramiv(renderer_data->quad_shader_program, GL_LINK_STATUS, &success);
-    if(!success)
+
+
+
+
+
     {
-        glGetProgramInfoLog(renderer_data->quad_shader_program, 512, NULL, info_log);
+        const char *vert_shader_source =
+            "#version 330 core\n"
+            "layout (location = 0) in vec2 a_pos;\n"
+            "layout (location = 1) in vec2 a_uv;\n"
+            "uniform mat4 mvp;\n"
+            "out vec2 uvs;\n"
+            "void main()\n"
+            "{\n"
+            "    uvs = a_uv;\n"
+            "    gl_Position = mvp * vec4(a_pos.x, a_pos.y, 0.0f, 1.0);\n"
+            "}\0";
+
+        const char *frag_shader_source =
+            "#version 330 core\n"
+            "uniform sampler2D texture0;\n"
+            "in vec2 uvs;"
+            "out vec4 frag_color;\n"
+            "void main()\n"
+            "{\n"
+            "    frag_color = texture(texture0, uvs);\n"
+            //"    frag_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+            //"    frag_color = vec4(uvs.x, uvs.y, 0.0f, 0.0f);\n"
+            "}\0";
+
+        renderer_data->quad_shader_program = make_shader(vert_shader_source, frag_shader_source);
     }
 
-    glDeleteShader(vert_shader);
-    glDeleteShader(frag_shader); 
-    check_gl_errors("deleting shaders");
+
+
+
+    {
+        const char *vert_shader_source =
+            "#version 330 core\n"
+            "layout (location = 0) in vec2 a_pos;\n"
+            "layout (location = 1) in vec2 a_scale;\n"
+            "layout (location = 2) in vec2 a_rotation;\n"
+            "layout (location = 3) in vec2 a_color;\n"
+
+            "uniform mat4 vp;\n"
+
+            "out VS_OUT\n"
+            "{\n"
+            "    mat4 mvp;\n"
+            "} vs_out;\n"
+
+            "void main()\n"
+            "{\n"
+            "    mat4 m = mat4(\n"
+            "       a_scale.x, 0.0f, 0.0f, 0.0f,\n"
+            "       0.0f, a_scale.y, 0.0f, 0.0f,\n"
+            "       0.0f, 0.0f, 1.0f, 0.0f,\n"
+            "       a_pos.x, a_pos.y, 0.0f, 1.0f\n"
+            "       );\n"
+            "    vs_out.mvp = vp * m;\n"
+            "    gl_Position = vec4(0.0f, 0.0f, 0.0f, 1.0);\n"
+            "}\0";
+
+#if 1
+        const char *geom_shader_source =
+        {
+            "#version 330 core\n"
+            "layout (points) in;\n"
+            "layout (triangle_strip, max_vertices = 4) out;\n"
+            "in VS_OUT\n"
+            "{\n"
+            "    mat4 mvp;\n"
+            "} gs_in[];\n"
+
+            "out vec2 uvs;\n"
+            
+            "void main() {\n"
+            "    mat4 mvp = gs_in[0].mvp;\n"
+            "    uvs = vec2(0.0f, 0.0f);\n"
+            "    gl_Position = mvp * (gl_in[0].gl_Position + vec4(-0.5f, -0.5f, 0.0f, 0.0f));\n"
+            "    EmitVertex();\n"
+
+            "    uvs = vec2(1.0f, 0.0f);\n"
+            "    gl_Position = mvp * (gl_in[0].gl_Position + vec4( 0.5f, -0.5f, 0.0f, 0.0f));\n"
+            "    EmitVertex();\n"
+
+            "    uvs = vec2(0.0f, 1.0f);\n"
+            "    gl_Position = mvp * (gl_in[0].gl_Position + vec4(-0.5f,  0.5f, 0.0f, 0.0f));\n"
+            "    EmitVertex();\n"
+
+            "    uvs = vec2(1.0f, 1.0f);\n"
+            "    gl_Position = mvp * (gl_in[0].gl_Position + vec4( 0.5f,  0.5f, 0.0f, 0.0f));\n"
+            "    EmitVertex();\n"
+
+            "    EndPrimitive();\n"
+            "}\n"
+        };
+#else
+        const char *geom_shader_source =
+        {
+            "#version 330 core\n"
+            "layout (points) in;\n"
+            "layout (points, max_vertices = 1) out;\n"
+            //"in mat4 mvp;\n"
+            "void main() {\n"
+            "    gl_Position = gl_in[0].gl_Position;\n"
+            "    EmitVertex();\n"
+
+            "    EndPrimitive();\n"
+            "}\n"
+        };
+#endif
+
+        const char *frag_shader_source =
+            "#version 330 core\n"
+            "uniform sampler2D texture0;\n"
+            "in vec2 uvs;"
+            "out vec4 frag_color;\n"
+            "void main()\n"
+            "{\n"
+            "    frag_color = texture(texture0, uvs);\n"
+            //"    frag_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+            //"    frag_color = vec4(uvs.x, uvs.y, 0.0f, 1.0f);\n"
+            "}\0";
+
+        renderer_data->particles_shader_program = make_shader(vert_shader_source, frag_shader_source, geom_shader_source);
+        check_gl_errors("particle shader");
+    }
 
 
     glEnable(GL_BLEND);
@@ -657,11 +836,43 @@ static void render()
     glClearColor(0.1f, 0.0f, 0.2f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+
+#if 0
     for(int i = 0; i < particle_data->num_particles; i++)
     {
-        Particle *p = &(particle_data->particles[i]);
-        render_quad(p->position, v2(1.0f, 1.0f) * p->scale);
+        RenderParticle *p = &(particle_data->render_particles[i]);
+        render_quad(p->position, p->scale);
     }
+#else
+    glUseProgram(renderer_data->particles_shader_program);
+
+
+    mat4 ndc_m_world = get_ndc_m_world();
+
+    GLint loc = glGetUniformLocation(renderer_data->particles_shader_program, "vp");
+    glUniformMatrix4fv(loc, 1, true, &(ndc_m_world[0][0]));
+    check_gl_errors("set uniform");
+
+
+    glBindVertexArray(renderer_data->particles_vao);
+    check_gl_errors("use vao");
+
+    glBindBuffer(GL_ARRAY_BUFFER, renderer_data->particles_vbo);
+    check_gl_errors("use vbo");
+    
+    glBufferSubData(GL_ARRAY_BUFFER, 0,
+            sizeof(RenderParticle) * particle_data->num_particles, particle_data->render_particles);
+    check_gl_errors("send particle data");
+
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, renderer_data->default_texture);
+
+    glDrawArrays(GL_POINTS, 0, particle_data->num_particles);
+
+    check_gl_errors("draw");
+ 
+#endif
 
 
     ImGui::Render();
